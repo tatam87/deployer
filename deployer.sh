e979c34a7348153c7d50babe47ee907f842e9178
tatam <s.stamatiou@mpass.gr>
2019-11-05 16:23:13 +0200

    adding some code and apache sample

--- deployer.sh
+++ deployer.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
 
+printf "Hello:\t\t$USER\n"
+
+user=$USER
+rsa=/home/$USER/.ssh/id_rsa
+
+printf " Please answer a few questions about the new deployment with y or n\n"
+
+read -p 'Will you use php? ' php
+read -p 'Will you use local mysql? ' mysql
+read -p 'Will you use new apache domain? ' apache
+read -p 'Please provide the domain to use? ' domain
+read -p 'Will you use backend? ' backend
+read -p 'Will you use frontend? ' frontend
+read -p 'Will you use cms? ' cms
+read -p 'Will you use proxy for backend? ' proxyb
+
+if [ $backend == y ] ; then
+		read -p 'please provide the cloning repository for backend? ' backendrepo
+	fi
+if [ $frontend == y ] ;	then
+		read -p 'please provide the cloning repository for backend? ' frontendrepo
+	fi
+if [ $cms == y ] ;   then
+                read -p 'please provide the cloning repository for backend? ' cmsrepo
+	fi
+
+
+
