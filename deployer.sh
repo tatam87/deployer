3188ccaccda075b72acd4f3792c2ec98a0451647
tatam <s.stamatiou@mpass.gr>
2019-11-06 20:02:34 +0200

    logic change and git configs

--- deployer.sh
+++ deployer.sh
@@ -2,19 +2,22 @@
 
 printf "Hello:\t\t$USER\n"
 installer="sudo apt-get update "
-apachios="sudo apt-get install -y apache2 && sudo a2enmod alias ssl headers proxy proxy_fcgi proxy_http proxy_html rewrite xml2enc && sudo systemctl restart apache2"
+apachios="sudo apt-get install -y apache2 && sudo a2enmod alias ssl headers proxy proxy_fcgi proxy_http proxy_html rewrite xml2enc && sudo systemctl restart apache2 && sudo add-apt-repository ppa:certbot/certbot && sudo apt-get update && sudo apt-get install -y certbot python-certbot-apache "
 rsa=/home/$USER/.ssh/id_rsa
 apasimple=apache_sample
-apaproxy=`cat apache_backproxy_sample`
+apaproxy=apache_backproxy_sample
 printf "Please answer a few questions about the new deployment with y or n:\n"
 read -p 'Please provide the environenment ex:prod/dev : ' env
 read -p 'What is the project name? ' pname
 read -p 'Please provide the domain? ' domain
+read -p 'Is that the first time deployng on this server? ' deployment
 read -p 'Will you use frontend? ' frontend
 
 if [ $frontend == y ] ; then
+    repos="${repos}front"
       printf " Example of repository: https://github.com/someuser/someproject.git\n"
       read -p 'Please provide the cloning repository for frontend: ' frontendrepo
+      read -p "Please provide the branch: "  fbranch
       read -p 'Will you use php? ' php
            if [ $php == y ] ; then
                     installer="${installer}&& sudo apt-get install -y php-fpm php-curl php-bcmath php-intl php-json php-mbstring php-mysql php-soap php-xml php-zip"
@@ -30,6 +33,7 @@ if [ $frontend == y ] ; then
 fi
 read -p 'Will you use backend server? ' backend
   if [ $backend == y ] ; then
+      repos="${repos}back"
         read -p 'Will you use local java? ' java
               if [ $java == y ] ; then
                 read -p 'Please specify which java 8/11: ' javaversion
@@ -51,13 +55,16 @@ read -p 'Will you use backend server? ' backend
         fi
         printf " Example of repository: https://github.com/someuser/someproject.git\n"
         read -p 'Please provide the cloning repository for backend: ' backendrepo
+        read -p "Please provide the branch: "  bbranch
         read -p 'Whats the backend port? ' backendport
     fi
 
 read -p 'Will you use cms? ' cms
   if [ $cms == y ] ;   then
+                repos="${repos}cms"
                 printf " Example of repository: https://github.com/someuser/someproject.git\n"
                 read -p 'Please provide the cloning repository for cms: ' cmsrepo
+                read -p "Please provide the branch: "  cbranch
                 read -p 'What is the cms alias? '  cmsalias
   fi
 
@@ -66,9 +73,31 @@ read -p 'Please provide the ssh user to connect: ' remoteuser
 read -p 'Please provide the ssh port to use: ' sshport
 
 path="\/var\/www\/$env\/$pname\/"
-ospath="/var/www/$env/$pname/"
+ospath="/var/www/"
 servicepath="/etc/systemd/system/$env-$pname.service"
 
 cat apache_sample | sed "s/domain/$domain/; s/path/$path/g" > $domain.conf
-
-#ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" $installer
+if [ $deployment == y ] ;   then
+ ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "$apachios && $installer && sudo mkdir -p $ospath/$env/$pname && sudo chown $remoteuser:www-date -R $ospath/$env && sudo chmod -R 775 $$ospath/$env/"
+  else
+    ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "$installer  && sudo mkdir -p $ospath/$env/$pname && sudo chown $remoteuser:www-data -R $ospath/$env/ && sudo chmod -R 775 $ospath/$env/ "
+  fi
+case $repos in
+    "front")
+      ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd /$ospath/$env/$pname/ git clone $frontendrepo"
+    ;;
+    "back")
+      ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd /$ospath/$env/$pname/ git clone $backendrepo"
+    ;;
+    "cms")
+      ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd /$ospath/$env/$pname/ git clone $cmsrepo"
+    ;;
+    "frontback")
+      ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd /$ospath/$env/$pname/ git clone $frontendrepo && git clone $backendrepo"
+    ;;
+    "frontbackcms")
+      ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd /$ospath/$env/$pname/ git clone $frontendrepo && git clone $backendrepo && git clone $cmsrepo"
+    ;;
+    *)
+  esac
+rm $domain.conf
