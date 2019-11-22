9f27df15aa55d140f294e11256e1ae3d4bfcff9a
tatam <s.stamatiou@mpass.gr>
2019-11-22 12:26:25 +0200

    hope to see working chamges plus fixes in logic adn sql

--- deployer.sh
+++ deployer.sh
@@ -65,10 +65,13 @@ read -e -i "y" -p 'Will you use backend server? ' backend
   fi
   read -e -i "n" -p 'Will you use RDS: ' rds
   if [ $rds == "y" ] ; then
-    read -p 'Please provide the RDS master password: ' rdspass
-    read -p 'Please provide the rdsuser you want to create: ' rdsuser
-    read -p 'Please provide the RDS user pasword you want to create: ' rdspass
+    read -p 'Please provide the RDS master user: ' rootuser
+    read -p 'Please provide the RDS master password: ' rootpasswd
+    read -p 'Please provide the rdsuser you want to create: ' mysqluser
+    read -p 'Please provide the RDS user pasword you want to create: ' sqluserpass
+    read -p 'Please provide the database name: ' bdshceme
     read -p 'Please provide the RDS host url: ' rdshost
+    read -p 'Please provide the RDS port: ' rdsport
   else
     read -e -i "y" -p 'Will you use mysql on the server? ' mysql
       if [ $mysql == "y" ] ; then
@@ -103,32 +106,41 @@ ospath="/var/www/"
 
 if [ $backend == "y" ] ;   then
   cat apache_backproxy_sample | sed "s|domain|$domain|g; s|backendport|$backendport|g; s|path|$path/$backdir|g" > $domain.conf
-  cat service_sample | sed "s|project|$pname|g; s|dir|$backdir|g; s|env|$env|g; s|pname|$pname|g; s|portc|$backendport|g" > $pname.service
+  cat service_sample | sed "s|project|$pname|g; s|dir|$backdir|g; s|env|$env|g; s|pname|$pname|g; s|portc|$backendport|g" > $pname-$env.service
 else
   cat apache_sample | sed "s|domain|$domain|g; s|backend|$path/$backdir|; s|path|$path|g" > $domain.conf
 fi
 
-if [ $deployment == y ] ;   then
- ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "$apachios && $installer && sudo mkdir -p $ospath/$pname/$env && sudo chown -R $remoteuser:$remoteuser $ospath/$pname/ && sudo chmod -R 775 $ospath/$pname/"
+if [ $deployment == "y" ] ;   then
+ ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "$apachios && $installer && sudo mkdir -p -v $ospath/$pname/$env && sudo chown -R $remoteuser:$remoteuser $ospath/$pname/ && sudo chmod -R 775 $ospath/$pname/"
 fi
 
-if  [ $mysql == y ] ; then
-  read -sp 'Please provide the root mysql password you have or created on deployment: ' rootpasswd
-  ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "mysql -uroot -p${rootpasswd}  <<-EOF
-  CREATE DATABASE $bdshceme;
-  CREATE USER $mysqluser@localhost IDENTIFIED BY $sqluserpass;
-  GRANT ALL PRIVILEGES ON $bdshceme.* TO $mysqluser@localhost;
-  FLUSH PRIVILEGES;
-  exit
-  EOF"
+if [ $rds == "y" ] ; then
+mysql -u$rootuser -p$rootpasswd -h$rdshost -p$rdsport  "<<EOF
+CREATE DATABASE $bdshceme;
+CREATE USER '$mysqluser'@'localhost' IDENTIFIED BY '$sqluserpass';
+GRANT ALL PRIVILEGES ON $bdshceme.* TO '$mysqluser'@'localhost';
+FLUSH PRIVILEGES;
+exit
+EOF"
+
+else
+  if  [ $mysql == "y" ] ; then
+    read -sp 'Please provide the root mysql password you have or created on deployment: ' rootpasswd
+    ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "mysql -uroot -p${rootpasswd}  <<EOF
+    CREATE DATABASE $bdshceme;
+    CREATE USER '$mysqluser'@'localhost' IDENTIFIED BY '$sqluserpass';
+    GRANT ALL PRIVILEGES ON $bdshceme.* TO '$mysqluser'@'localhost';
+    FLUSH PRIVILEGES;
+    exit
+    EOF"
+  fi
 fi
-ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "$installer  && sudo mkdir -p $ospath/$pname/$env && sudo chown $remoteuser:$remoteuser -R $ospath/$pname/ && sudo chmod -R 775 $ospath/$pname/ && sudo certbot certonly --apache -d$domain"
+ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "$installer  && sudo mkdir -p -v $ospath/$pname/$env && sudo chown $remoteuser:$remoteuser -R $ospath/$pname/ && sudo chmod -R 775 $ospath/$pname/ && sudo certbot certonly --apache -d$domain"
 
 if [[ $repos =~ "front" ]] ;   then
     ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd $ospath/$pname/$env/ && git clone $frontendrepo && cd $frontdir && git checkout $fbranch && cat src/config/config.js.sample | sed 's|/path/to/api|/api/v1/; s|http://localhost:8081|https://$domain|; s|dev|$env|' > src/config/config.js"
-      if [ $yarnf == y ] ; then
-            ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd $ospath/$pname/$env/$frontdir/ "
-      fi
+    ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd $ospath/$pname/$env/$frontdir/ && sh deploy-$env.sh"
     scp -P$sshport $domain.conf $remoteuser@$domain:~/
     ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "sudo mv $domain.conf /etc/apache2/sites-available/ && sudo a2ensite $domain && sudo systemctl reload apache2"
     rm $domain.conf
@@ -136,9 +148,9 @@ fi
 
 if [[ $repos =~ "back" ]] ; then
       ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "cd $ospath/$pname/$env/ && git clone $backendrepo && cd $backdir && git checkout $bbranch && cat src/main/resources/application.yaml.dist | sed 's|database_name|$bdshceme|; s|database_user|$mysqluser|; s|database_password|$sqluserpass|; s|spring_profile|$spring_profile|' > src/main/resources/application.yaml"
-      scp -P$sshport $pname.service  $remoteuser@$domain:~/
-      ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "sudo mv $pname.service /etc/systemd/system/ && sudo systemctl daemon-reload && sudo systemctl start $pname"
-      rm $pname.service
+      scp -P$sshport $pname-$env.service  $remoteuser@$domain:~/
+      ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=6776}" "sudo mv $pname-$env.service /etc/systemd/system/ && sudo systemctl daemon-reload && cd $ospath/$pname/$env/$bbranch && sh deploy-$env.sh"
+      rm $pname-$env.service
 fi
 
 if [[ $repos =~ "cms" ]] ; then
