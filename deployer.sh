2bd249661123c4260098129c2941b10822f6b2a7
tatam <s.stamatiou@mpass.gr>
2019-12-09 19:58:58 +0200

    varius changes on logical questions

--- deployer.sh
+++ deployer.sh
@@ -11,48 +11,63 @@ rsa=/home/$USER/.ssh/id_rsa
 
 apasimple=apache_sample
 apaproxy=apache_backproxy_sample
-
 printf "\n\033[0;33mPlease answer the questions for deployment with y or n or as suggested. \033[0m\n"
 read -e -i "prod" -p 'Please provide the environment type dev/stage/prod: ' env
 read -e -i "someproject" -p 'What is the project name? ' pname
 read -e -i "test.test.com" -p 'Please provide the domain? ' domain
 read -e -i "ubuntu" -p 'Please provide the ssh user to connect: ' remoteuser
-read -e -i "6776" -p 'Please provide the ssh port to use: ' sshport
+while  [[ $sshport !=  [0-9] ]]; do
+  read -e -i "6776" -p 'Please provide the ssh port to use: ' sshport
+done
 sshd="ssh -tt "${remoteuser:=ubuntu}"@$domain -p"${sshport:=22}""
-read -n1 -e -i "y" -p 'Is that the first time deployng on this server? ' deployment
+while  [[ $deployment !=  [YyNn] ]]; do
+  read -n1 -e -i "y" -p 'Is that the first time deployng on this server? ' deployment
+done
 echo "----------------------------"
-read -n1 -e -i "y" -p 'Will you use frontend? ' frontend
-
+while  [[ $deployment !=  [YyNn] ]]; do
+  read -n1 -e -i "y" -p 'Will you use frontend? ' frontend
+done
 if [[ $frontend == [Yy] ]] ; then
     repos="${repos}front"
       printf " Example of repository: https://github.com/someuser/someproject.git\n"
       read -p 'Please provide the cloning repository for frontend: ' frontendrepo
       frontdir=`echo $frontendrepo | rev | cut -d / -f1 | rev|cut -d . -f1`
       read -e -i "$env" -p "Please provide the branch: "  fbranch
-      read -n1 -e -i "y" -p 'Will you use npm? ' npm
+      while  [[ $npm !=  [YyNn] ]]; do
+       read -n1 -e -i "y" -p 'Will you use npm? ' npm
+      done
         if [[ $npm == [Yy] ]]  ; then
           installer="${installer} && sudo apt-get install -y npm nodejs && sudo npm -g install n && sudo n latest && sudo npm -g install yarn"
         fi
 fi
 
 echo "----------------------------"
-
+while  [[ $backend !=  [YyNn] ]]; do
 read -n1 -e -i "y" -p 'Will you use backend server? ' backend
+done
   if [[ $backend == [Yy] ]] ; then
       repos="${repos}back"
-      read -e -i "n" -p 'Will you use php? ' php
+      while  [[ $php !=  [YyNn] ]]; do
+        read -e -i "n" -p 'Will you use php? ' php
+      done
            if [[ $php == [Yy] ]] ; then
               installer="${installer}&& sudo apt-get install -y php-fpm php-curl php-bcmath php-intl php-json php-mbstring php-mysql php-soap php-xml php-zip"
-              read -n1 -e -i "y" -p 'Will you use composer? ' composer
+              while  [[ $composer !=  [YyNn] ]]; do
+                read -n1 -e -i "y" -p 'Will you use composer? ' composer
+              done
                 if [[ "$composer" == [Yy] ]] ; then
                   installer="${installer} composer "
                 fi
           fi
-        read -n1 -e -i "y" -p 'Will you use java? ' java
+        while  [[ $java !=  [YyNn] ]]; do
+          read -n1 -e -i "y" -p 'Will you use java? ' java
+        done
  fi
 
   if [[ $java == [Yy] ]] ; then
-    read -n2 -e -i "11" -p 'Please specify which java 8/11: ' javaversion
+    while [[ $javaversion !=  @(8|11)  ]]; do
+      read -n2 -e -i "11" -p 'Please specify which java 8/11: ' javaversion
+    done
     installer="${installer} && sudo apt-get install -y openjdk-$javaversion-jdk maven"
     release=`$sshd "hostnamectl | grep Operating | sed 's/[^0-9]//g' | head -c 2"`
 
@@ -61,7 +76,9 @@ read -n1 -e -i "y" -p 'Will you use backend server? ' backend
       fi
     read -e -i "prod" -p 'Please provide the spring profile: ' spring_profile
   fi
-  read -n1 -e -i "n" -p 'Will you use RDS: ' rds
+  while  [[ $rds !=  [YyNn] ]]; do
+    read -n1 -e -i "n" -p 'Will you use RDS: ' rds
+  done
   if [[ $rds == [Yy] ]] ; then
     read -p 'Please provide the RDS master user: ' rootuser
     read -sp 'Please provide the RDS master password: ' rootpasswd
@@ -71,7 +88,9 @@ read -n1 -e -i "y" -p 'Will you use backend server? ' backend
     read -p 'Please provide the RDS host url: ' rdshost
     read -p 'Please provide the RDS port: ' rdsport
   else
-    read -n1 -e -i "y" -p 'Will you use mysql on the server? ' mysql
+    while  [[ $mysql !=  [YyNn] ]]; do
+      read -n1 -e -i "y" -p 'Will you use mysql on the server? ' mysql
+    done
       if [[ $mysql == [Yy] ]] ; then
         installer="${installer} && sudo apt-get install -y mysql-server"
         read -p 'Please provide sql user: ' mysqluser
@@ -83,11 +102,13 @@ read -n1 -e -i "y" -p 'Will you use backend server? ' backend
   read -p 'Please provide the cloning repository for backend: ' backendrepo
   backdir=`echo $backendrepo | rev | cut -d / -f1 | rev|cut -d . -f1`
   read -e -i "$env" -p "Please provide the branch: "  bbranch
-  read -e -i "5000" -p 'Whats the backend port? ' backendport
-
+  while  [[ $backendport !=  [0-9] ]]; do
+    read -e -i "5000" -p 'Whats the backend port? ' backendport
+  done
 echo "----------------------------"
-
-read -n1 -e -i "n" -p 'Will you use cms? ' cms
+while  [[ $cms !=  [YyNn] ]]; do
+  read -n1 -e -i "n" -p 'Will you use cms? ' cms
+done
   if [[ $cms == [Yy] ]] ;   then
       repos="${repos}cms"
       printf " Example of repository: https://github.com/someuser/someproject.git\n"
